a4b5f1d8695f9a86131ddd6baf8b6a692b932ac1
Shane McDermed <shane.mcdermed@tapinfluence.com>
2014-01-03 23:22:40 -0800

    updated

---
+++ assets/js/app/aura_components/list-songs-in-party/main.js
@@ -0,0 +1,46 @@
+function getUrlVars()
+{
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
+
+Hull.component({
+  datasources: {songs: function() {return this.getSongs();}}, //List data sources you want to use here
+  templates: ['results'], //Declare the template names you use here
+  refreshEvents: ['search.refresh'],
+  initialize: function () {
+    this.partyId = getUrlVars()['id'] || false;
+
+    this._filter = Hull.util._.filter;
+
+    this.sandbox.on('search.find', Hull.util._.bind(this.setTerm, this));
+  }, //Called at component initialization
+  beforeRender: function (data, errors) {
+    console.log('About to render search results with data', data);
+    if(!data.songs) return false;
+    return data;
+
+  }, //Called after datasources are resolved
+  afterRender: function () {
+  }, //Called after template has been rendered - put jQuery plugin calls here
+  /* append your own components methods */
+  getSongs: function() {
+    console.log('Get Zee songs', arguments);
+    if(!this.searchTerm) return false;
+
+    var dfd = Hull.data.deferred();
+
+    $.getJSON('/party/' + this.partyId,
+      function(trackData) {
+        if(trackData.playlist)
+        dfd.resolve(trackData.playlist);
+      });
+  }
+});
