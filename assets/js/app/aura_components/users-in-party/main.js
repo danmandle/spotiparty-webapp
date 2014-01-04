b3629b9dc36f32289023ff6351555ebfa288a9a4
Shane McDermed <shane.mcdermed@tapinfluence.com>
2014-01-04 13:15:03 -0800

    slkadfj

--- assets/js/app/aura_components/users-in-party/main.js
+++ assets/js/app/aura_components/users-in-party/main.js
@@ -1,12 +1,21 @@
 Hull.component({
-  datasources: {}, //List data sources you want to use here
+  datasources: {users: function() {return this.getUsers();}}, //List data sources you want to use here
   templates: ['users-in-party'], //Declare the template names you use here
   refreshEvents: ['online.users.change'],
   initialize: function () {
-
+    this.partyId = getUrlVars()['id'] || this.api.model('me').attributes.extra.partyId;
   }, //Called at component initialization
   beforeRender: function (data, errors) {}, //Called after datasources are resolved
   afterRender: function () {
     // alert('refresh')
   }, //Called after template has been rendered - put jQuery plugin calls here
+  getUsers: function() {
+    return $.ajax({
+      url: '/user/atParty',
+      type: 'GET',
+      data: {
+        partyId: this.partyId
+      }
+    });
+  }
 });
--- assets/js/app/templates/templates-compiled.js
+++ assets/js/app/templates/templates-compiled.js
@@ -139,8 +139,23 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
 this["Hull"]["templates"]["users-in-party/users-in-party"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
+  var buffer = "", stack1, stack2, functionType="function", escapeExpression=this.escapeExpression, self=this;
+
+function program1(depth0,data) {
   
+  var buffer = "", stack1;
+  buffer += "\n<li>"
+    + escapeExpression(((stack1 = (depth0 && depth0.name)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + " - "
+    + escapeExpression(((stack1 = (depth0 && depth0.id)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + "</li>\n";
+  return buffer;
+  }
 
-
-  return "user";
+  buffer += escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.me)),stack1 == null || stack1 === false ? stack1 : stack1.name)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + "\n\n";
+  stack2 = helpers.each.call(depth0, (depth0 && depth0.users), {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
+  if(stack2 || stack2 === 0) { buffer += stack2; }
+  buffer += "\n";
+  return buffer;
   });
