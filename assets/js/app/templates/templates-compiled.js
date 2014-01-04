7aefeb0e81e0aa5be491ff28b60a0891f764955d
Shane McDermed <shane.mcdermed@tapinfluence.com>
2014-01-03 23:49:10 -0800

    argh. It's late

--- assets/js/app/templates/templates-compiled.js
+++ assets/js/app/templates/templates-compiled.js
@@ -61,7 +61,11 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
 function program1(depth0,data) {
   
   var buffer = "", stack1;
-  buffer += "\n      <tr>\n        <td>";
+  buffer += "\n      <tr>\n        <td data-songId=\"";
+  if (stack1 = helpers.songId) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = (depth0 && depth0.songId); stack1 = typeof stack1 === functionType ? stack1.call(depth0, {hash:{},data:data}) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "\">";
   if (stack1 = helpers.songName) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = (depth0 && depth0.songName); stack1 = typeof stack1 === functionType ? stack1.call(depth0, {hash:{},data:data}) : stack1; }
   buffer += escapeExpression(stack1)
@@ -69,13 +73,14 @@ function program1(depth0,data) {
     + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.user)),stack1 == null || stack1 === false ? stack1 : stack1.picture)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
     + "</td>\n        <td>"
     + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.user)),stack1 == null || stack1 === false ? stack1 : stack1.name)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + "</td>\n      </tr>\n  </tbody>\n</table>\n";
+    + "</td>\n      </tr>\n";
   return buffer;
   }
 
   buffer += "<table>\n  <thead>\n    <tr>\n      <th>Artist</th>\n      <th>pictureUrl</th>\n      <th>UserName</th>\n    </tr>\n  </thead>\n  <tbody>\n";
   stack1 = helpers.each.call(depth0, (depth0 && depth0.songs), {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
   if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\n</tbody>\n</table>";
   return buffer;
   });
 
