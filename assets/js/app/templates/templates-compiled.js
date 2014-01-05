755416f844e5242ddd156c2c539c7421f1c4aa5f
Brian Shaffer <brianshaffer@Macbook-2.local>
2014-01-04 18:57:19 -0700

    Tons of UI tweaks

--- assets/js/app/templates/templates-compiled.js
+++ assets/js/app/templates/templates-compiled.js
@@ -35,22 +35,28 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
 this["Hull"]["templates"]["list-parties/template"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
-  var stack1, functionType="function", escapeExpression=this.escapeExpression, self=this;
+  var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression, self=this;
 
 function program1(depth0,data) {
   
   var buffer = "", stack1;
-  buffer += "\n  <div>";
+  buffer += "\n  <div class=\"party\">\n  	<a href=\"#\" onclick=\"$('body').trigger('join.party', ";
+  if (stack1 = helpers.id) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = (depth0 && depth0.id); stack1 = typeof stack1 === functionType ? stack1.call(depth0, {hash:{},data:data}) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "); return false;\">\n  	<span class=\"pull-right join-party\">Join Party <i class=\"icon-right-open-mini\"></i></span>\n  	<h2>";
   if (stack1 = helpers.partyName) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = (depth0 && depth0.partyName); stack1 = typeof stack1 === functionType ? stack1.call(depth0, {hash:{},data:data}) : stack1; }
   buffer += escapeExpression(stack1)
-    + "</div>\n";
+    + "</h2>\n 	 	<h3>Las Vegas, NV</h3>\n 	 	</a>\n	</div>\n";
   return buffer;
   }
 
+  buffer += "<div class=\"parties\">\n";
   stack1 = helpers.each.call(depth0, (depth0 && depth0.parties), {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
-  if(stack1 || stack1 === 0) { return stack1; }
-  else { return ''; }
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\n</div>";
+  return buffer;
   });
 
 this["Hull"]["templates"]["list-songs-in-party/list-songs-in-party"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
@@ -68,11 +74,13 @@ function program1(depth0,data) {
     + "\">\n      <div class=\"order\">\n      ";
   stack2 = helpers['if'].call(depth0, ((stack1 = data),stack1 == null || stack1 === false ? stack1 : stack1.index), {hash:{},inverse:self.program(4, program4, data),fn:self.program(2, program2, data),data:data});
   if(stack2 || stack2 === 0) { buffer += stack2; }
-  buffer += "\n    </div>\n    <div class=\"voting\">\n      <a href=\"#\" class=\"up\"><i class=\"icon-up\"></i></a>\n      <a href=\"#\" class=\"down\"><i class=\"icon-down\"></i></a>\n    </div>\n      <img src=\""
+  buffer += "\n    </div>\n    <div class=\"voting\">\n      <a href=\"#\" class=\"up\"><i class=\"icon-up\"></i></a>\n      <a href=\"#\" class=\"down\"><i class=\"icon-down\"></i></a>\n    </div>\n    <div class=\"userPhoto ttip\" data-tooltip=\"Added by "
+    + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.user)),stack1 == null || stack1 === false ? stack1 : stack1.name)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + "\">\n      <img src=\""
     + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.user)),stack1 == null || stack1 === false ? stack1 : stack1.picture)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
     + "\" alt=\""
     + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.user)),stack1 == null || stack1 === false ? stack1 : stack1.name)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + "\" class=\"userPhoto\" />\n      <h2>";
+    + "\" />\n    </div>\n      <h2>";
   if (stack2 = helpers.songName) { stack2 = stack2.call(depth0, {hash:{},data:data}); }
   else { stack2 = (depth0 && depth0.songName); stack2 = typeof stack2 === functionType ? stack2.call(depth0, {hash:{},data:data}) : stack2; }
   buffer += escapeExpression(stack2)
@@ -131,7 +139,7 @@ function program1(depth0,data,depth1) {
     + escapeExpression(((stack1 = ((stack1 = ((stack1 = (depth0 && depth0.artists)),stack1 == null || stack1 === false ? stack1 : stack1[0])),stack1 == null || stack1 === false ? stack1 : stack1.name)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
     + "\" data-album-name=\""
     + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.album)),stack1 == null || stack1 === false ? stack1 : stack1.name)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + "\">\n      <h2>";
+    + "\">\n      <i class=\"icon-plus-circled plus-icon\"></i>\n      <h2>";
   if (stack2 = helpers.name) { stack2 = stack2.call(depth0, {hash:{},data:data}); }
   else { stack2 = (depth0 && depth0.name); stack2 = typeof stack2 === functionType ? stack2.call(depth0, {hash:{},data:data}) : stack2; }
   buffer += escapeExpression(stack2)
